73b88f7778d14e940bb0d34a021549f5bdb5ebac
artalcb <94609283+artalcb@users.noreply.github.com>
2025-11-03 19:02:11 +0100

    Js functionality

---
+++ js/index.js
@@ -0,0 +1,40 @@
+document.querySelectorAll('.link-js').forEach(link => {
+  link.addEventListener('click', e => {
+    if (e.metaKey || e.ctrlKey || e.shiftKey || e.altKey) return;
+
+    e.preventDefault();
+    link.style.pointerEvents = 'none';
+
+     // Find the .starter div inside the clicked link
+    const starterDiv = link.querySelector('.starter');
+    if (starterDiv) {
+      starterDiv.classList.remove('treecko'); 
+      starterDiv.classList.remove('torchic'); 
+      starterDiv.classList.remove('mudkip'); 
+      
+    }
+
+    const rect = link.getBoundingClientRect();
+    const startX = rect.left + rect.width / 2; // center X
+    const startY = rect.top + rect.height / 2; // center Y
+
+    // Create flying PNG
+    const flyingImg = document.createElement('img');
+    flyingImg.src = "img/pokeballpixel.png";
+    flyingImg.className = 'pokethrow';
+    flyingImg.style.left = `${startX-120}px`;
+    flyingImg.style.top = `${startY+60}px`;
+    starterDiv.appendChild(flyingImg);
+
+    
+    setTimeout(() => {
+      flyingImg.remove(); 
+      starterDiv.querySelector('img').setAttribute('src','img/smoke.gif')
+
+    }, 500); 
+    
+    setTimeout(() => {
+      window.location.href = link.href;
+    }, 750);
+  });
+});
